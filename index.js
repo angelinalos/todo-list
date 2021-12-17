6a9bb1e43434a3ec76408bd490a53eaca8ec28cc
angelinalos <newyorksquirrel33@gmail.com>
2021-12-17 03:48:20 +0300

    feat(todo): create main function

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ function createHeader() {
     return header
   }
 
+  // Main
+function createMain(tasks) {
+    const main = createElement("main", "main flex flex-col mx-9 my-3 gap-6")
+    const input = createElement("input", "rounded-full border-2 border-lime-500 self-center shadow-sm shadow-lime-500 bg-black text-gray-100 px-2 lg:px-6")
+    input.id = 'enter'
+    main.append(input)
+    tasks.forEach((task) => {
+      const card = createCard(task)
+      main.append(card)
+    })
+    return main
+  }
+
 // Elements
 function createElement(tag, className, text = "") {
     const element = document.createElement(tag)
