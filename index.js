d86e56b9d5847c770fbebc36354f68e829cba1b4
angelinalos <newyorksquirrel33@gmail.com>
2021-12-17 03:47:17 +0300

    feat(todo): create header function

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const root = document.querySelector("#root") 
+const header = createHeader()
+
+//   Header
+function createHeader() {
+    const header = createElement(
+      "header",
+      "flex flex-col gap-7 "
+    )
+    const btnAdd = createElement("button", "text-gray-100 rounded-full p-3 shadow-md shadow-lime-500 animate-pulse", "Add")
+    const btnDelete = createElement("button", "text-gray-100 rounded-full p-3 shadow-md shadow-fuchsia-500", "Delete All")
+    btnDelete.id = "delete"
+    btnAdd.id = "add"
+    header.append(btnAdd, btnDelete)
+    return header
+  }
 
 // Elements
 function createElement(tag, className, text = "") {
